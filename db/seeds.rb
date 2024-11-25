2c7fd63cf22ae39ff4729f2636f67cb0ea14072a
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-11-25 10:37:19 +0000

    feat: Add data for development usage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Create two users
+User.create(
+  [
+    { email: "jdoe@email.com", username: "jdoe", password: "password" },
+    { email: "sally@email.com", username: "sally", password: "rockyou" }
+  ]
+)
+
+# Use 'jdoe' to create a post
+jdoe = User.find_by(username: "jdoe")
+
+post = jdoe.posts.create(title: "My first post", content: "This is the post content")
+
+# Make 'sally' comment on the post
+sally = User.find_by(username: "sally")
+
+comment = post.comments.create(user: sally, body: "Nice post")
